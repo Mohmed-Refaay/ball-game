5cb5ef9ec813cced65cb9f03aec0506e5d6a3d00
Mohamed Amr Refaay <mohamedamrrefaay@gmail.com>
2024-05-19 18:01:34 +0300

    add camera controls

--- src/Components/Experience/Player.tsx
+++ src/Components/Experience/Player.tsx
@@ -3,23 +3,32 @@ import { useKeyboard } from "./controls/useControls";
 import { Triplet, useSphere } from "@react-three/cannon";
 import { Vector3 } from "three";
 import { useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { OrbitControls as OB } from "three-stdlib";
 
 const direction = new Vector3();
 
 const x = new Vector3();
 const y = new Vector3();
 
-const speed = -5;
+const MOVE_SPEED = -5;
+
+const JUMP_VELOCITY = 5;
+
+const CAMERA_TO_PLAYER_DIST = 5;
+
+const CAMERA_LERP = 0.1;
 
 export function Player() {
   const actions = useKeyboard();
+  const controls = useRef<OB>(null);
 
   const args: JSX.IntrinsicElements["sphereGeometry"]["args"] = [
     1, 64, 64,
   ];
 
   const [ref, api] = useSphere(() => ({
-    mass: 100,
+    mass: 1,
     position: [0, 5, 0],
     type: "Dynamic",
     // @ts-ignore
@@ -35,7 +44,47 @@ export function Player() {
     return unsubscribe;
   }, [api]);
 
-  useFrame(() => {
+  const position = useRef<Triplet>([0, 0, 0]);
+
+  useEffect(() => {
+    const unsubscribe = api.position.subscribe((p) => {
+      position.current = p;
+    });
+    return unsubscribe;
+  }, [position]);
+
+  useFrame(({ camera }) => {
+    const directionCamraToPlayer = new Vector3().subVectors(
+      camera.position,
+      new Vector3(
+        position.current[0],
+        position.current[1],
+        position.current[2],
+      ),
+    );
+
+    camera.position.lerp(
+      new Vector3(
+        position.current[0],
+        position.current[1],
+        position.current[2],
+      ).add(
+        directionCamraToPlayer
+          .normalize()
+          .multiplyScalar(CAMERA_TO_PLAYER_DIST),
+      ),
+      CAMERA_LERP,
+    );
+
+    controls.current?.target.lerp(
+      new Vector3(
+        position.current[0],
+        position.current[1],
+        position.current[2],
+      ),
+      CAMERA_LERP,
+    );
+
     x.set(
       0,
       0,
@@ -47,15 +96,30 @@ export function Player() {
       0,
       0,
     );
-    direction.subVectors(x, y).normalize().multiplyScalar(speed);
+    direction
+      .subVectors(x, y)
+      .normalize()
+      .multiplyScalar(MOVE_SPEED)
+      .applyEuler(camera.rotation);
 
     api.velocity.set(direction.x, velocity.current[1], direction.z);
+
+    if (actions.jump) {
+      api.velocity.set(
+        velocity.current[0],
+        JUMP_VELOCITY,
+        velocity.current[2],
+      );
+    }
   });
 
   return (
-    <mesh ref={ref as any}>
-      <sphereGeometry args={args} />
-      <meshStandardMaterial color="red" />
-    </mesh>
+    <>
+      <mesh ref={ref as any}>
+        <sphereGeometry args={args} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+      <OrbitControls ref={controls} />
+    </>
   );
 }
--- src/Components/Experience/index.tsx
+++ src/Components/Experience/index.tsx
@@ -1,22 +1,36 @@
 "use client";
 import { OrbitControls, useTexture } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
 import { Player } from "./Player";
-import { Physics } from "@react-three/cannon";
+import { Physics, Triplet } from "@react-three/cannon";
 import { Ground } from "./Ground";
+
+const GROUNDS = [
+  { size: 5, position: [0, 0, 0] },
+  { size: 5, position: [0, 0, 5] },
+  { size: 5, position: [0, 2, 10] },
+  { size: 5, position: [0, 0, -5] },
+  { size: 5, position: [5, 0, 0] },
+  { size: 5, position: [5, 0, 0] },
+  { size: 5, position: [-5, 0, 0] },
+];
 
 export function Experience() {
   return (
     <Canvas>
-      <Physics>
-        <Ground size={5} />
+      <Physics gravity={[0, -20, 0]}>
+        {GROUNDS.map((props, i) => (
+          <Ground
+            key={i}
+            size={props.size}
+            position={props.position as Triplet}
+          />
+        ))}
         <Player />
       </Physics>
 
       <ambientLight />
       <directionalLight />
-      <OrbitControls />
     </Canvas>
   );
 }
